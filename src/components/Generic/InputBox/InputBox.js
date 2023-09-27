2b88c6bdd5fb1b60fc35978482c73a6f537e947b
Josh <lolhongy3@gmail.com>
2023-09-27 20:33:43 +1000

    reusable input box

--- src/components/Generic/InputBox/InputBox.js
+++ src/components/Generic/InputBox/InputBox.js
@@ -1,10 +1,19 @@
 import React from "react";
+import styled from "styled-components";
 
-const InputBox = () => {
+const InputBox = ({ height, width, background, color}) =>{
+
+    const Container = styled.input.attrs()`
+    height:${height}px;
+    width:${width}px;
+    background:${background};
+    color:${color};
+    `
+
     return(
-        <input type="text">
-        </input>
+    <Container/>
     )
 }
 
 export default InputBox
+
